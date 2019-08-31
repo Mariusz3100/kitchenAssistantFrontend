b469d68710355abae77aa4bd24c852a9c4bbc259
M <armaniel667@gmail.com>
2019-08-31 08:04:19 +0200

    changes to app component.

---
+++ src/app/Token.ts
@@ -0,0 +1,10 @@
+export class Token {
+  text: string;
+  lemma: string;
+  tag: string;
+
+
+
+
+}
+// {"text": "2", "lemma": "2", "tag": "CD"}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TokenizerComponent } from './tokenizer/tokenizer.component';
+import { AppRoutingModule } from './app-routing.module';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TokenizerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
