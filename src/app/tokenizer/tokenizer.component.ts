af27114305513a735642807514f83bc73cb6e827
M <armaniel667@gmail.com>
2019-08-31 07:59:23 +0200

    Initial stub for tokenizer component

---
+++ src/app/tokenizer/tokenizer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tokenizer',
+  templateUrl: './tokenizer.component.html',
+  styleUrls: ['./tokenizer.component.css']
+})
+export class TokenizerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
